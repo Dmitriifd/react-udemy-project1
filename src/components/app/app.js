4a271f881f48a6fcd5eb7d3d1124ced615019967
Dmitriifd <idimka9@gmail.com>
2022-10-07 19:59:40 +0500

    свойства компонентов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,31 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel'
+import AppFilter from '../app-filter/app-filter'
+import EmployeesList from '../employees-list/employees-list'
+import EmployeesAddForm from '../employees-add-form/employees-add-form'
 
-import './app.css';
+import './app.css'
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+	const data = [
+		{ name: 'John Smith', salary: '800', increase: false, id: 1 },
+		{ name: 'Alex Minik.', salary: '3000', increase: true, id: 2 },
+		{ name: 'Carl With.', salary: '5000', increase: false, id: 3 },
+	]
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<AppInfo />
+
+			<div className='search-panel'>
+				<SearchPanel />
+				<AppFilter />
+			</div>
+
+			<EmployeesList data={data}/>
+			<EmployeesAddForm />
+		</div>
+	)
 }
 
-export default App;
+export default App
